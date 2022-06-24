b83851258ee5d2f4c7f1e0bc1e67e9ccc03d1c6b
Vitalii Mytenko <vetalmit@gmail.com>
2022-06-24 17:01:13 +0300

    Update csv file generation

--- fake_data_generator/tasks.py
+++ fake_data_generator/tasks.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 from typing import Any, List
 
 from faker import Faker
@@ -9,9 +10,12 @@ fake = Faker()
 
 
 def make_csv(data: List[Any]):
+    current_time = datetime.now().strftime("%H-%M-%S")
     headers: List[str] = ["name", "phone", "email"]
-    with open(file="fake_data.csv", mode="w", encoding="UTF-8") as csv_file:
-        writer = csv.writer(csv_file)
+    with open(file=f"data_files/fake_data_{current_time}.csv", mode="w", encoding="UTF-8") as csv_file:
+        writer = csv.writer(
+            csv_file, delimiter=";", quotechar='"', quoting=csv.QUOTE_MINIMAL
+        )
         writer.writerow(headers)
         writer.writerows(data)
     return csv_file
