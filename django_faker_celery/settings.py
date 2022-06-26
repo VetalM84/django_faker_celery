ab2a768541f8534eda1ff7dffd3681b1b5eb957f
Vitalii Mytenko <vetalmit@gmail.com>
2022-06-26 11:29:34 +0300

    Heroku bug fix static files

--- django_faker_celery/settings.py
+++ django_faker_celery/settings.py
@@ -13,9 +13,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -70,7 +68,7 @@ CELERY_TIMEZONE = 'Europe/Amsterdam'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
