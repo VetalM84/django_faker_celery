d078f823195e95cf2e96cc723b17e8dede93ddae
Vitalii Mytenko <vetalmit@gmail.com>
2022-06-30 12:38:15 +0300

    Upload user files to AWS

--- django_faker_celery/settings.py
+++ django_faker_celery/settings.py
@@ -144,17 +144,20 @@ if USE_S3:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
     # s3 static settings
-    AWS_LOCATION = 'static'
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    STATIC_LOCATION = 'static'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
+    STATICFILES_STORAGE = 'django_faker_celery.storage_backends.StaticStorage'
+    # s3 public media settings
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'django_faker_celery.storage_backends.PublicMediaStorage'
 else:
     STATIC_URL = '/staticfiles/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    MEDIA_URL = '/mediafiles/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
-MEDIA_URL = '/mediafiles/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- fake_data_generator/tasks.py
+++ fake_data_generator/tasks.py
@@ -1,9 +1,11 @@
 """Tasks for Celery"""
-
 import csv
+import logging
 import os
 from typing import Any, List
 
+import boto3
+from botocore.exceptions import ClientError
 from faker import Faker
 
 from django_faker_celery.celery import app
@@ -11,22 +13,46 @@ from django_faker_celery.celery import app
 fake = Faker()
 
 
+def upload_file(file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client("s3")
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
 @app.task
 def make_csv(data: List[Any], task_id: str):
     """Produce csv file with generated fake data and name it as task id."""
     headers: List[str] = ["name", "phone", "email"]
-    try:
-        os.mkdir(path="tmp")
-    except FileExistsError:
-        print("Directory already exists")
 
-    file_path = os.path.normpath(f"tmp/{task_id}.csv")
+    file_path = os.path.normpath(os.path.basename(f"{task_id}.csv"))
+    print(f"File path: {file_path}")
     with open(file=file_path, mode="w", encoding="UTF-8", newline="") as csv_file:
         writer = csv.writer(
             csv_file, delimiter=";", quotechar='"', quoting=csv.QUOTE_MINIMAL
         )
         writer.writerow(headers)
         writer.writerows(data)
+        upload_file(
+            file_name=csv_file,
+            bucket=os.getenv("AWS_STORAGE_BUCKET_NAME"),
+        )
     return True
 
 
