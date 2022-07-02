91a120ff2831c32ea07630de838363a584886819
Vitalii Mytenko <vetalmit@gmail.com>
2022-07-02 20:58:49 +0300

    Bugfix

--- django_faker_celery/settings.py
+++ django_faker_celery/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'fake_data_generator.apps.FakeDataGeneratorConfig',
-    'django_celery_results',
     'storages',
 ]
 
